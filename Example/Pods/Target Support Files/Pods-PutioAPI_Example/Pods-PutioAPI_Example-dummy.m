3f6893eeb54b830965bcacc75a06ff6fb4351933
Altay Aydemir <altayhan.aydemir@klarna.com>
2019-10-13 10:26:31 +0200

    Add Example/Pods to .gitignore

--- Example/Pods/Target Support Files/Pods-PutioAPI_Example/Pods-PutioAPI_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PutioAPI_Example : NSObject
-@end
-@implementation PodsDummy_Pods_PutioAPI_Example
-@end
--- Example/Pods/Target Support Files/Pods-PutioAPI_Tests/Pods-PutioAPI_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PutioAPI_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PutioAPI_Tests
-@end
