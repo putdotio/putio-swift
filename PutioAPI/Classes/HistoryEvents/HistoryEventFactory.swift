200047c96fe202de1a4519b837b1eb9b716201ff
BatuAksoy <aksoyb15@itu.edu.tr>
2020-09-18 12:15:46 +0300

    add zip created event to factory

--- PutioAPI/Classes/HistoryEvents/HistoryEventFactory.swift
+++ PutioAPI/Classes/HistoryEvents/HistoryEventFactory.swift
@@ -11,11 +11,11 @@ open class PutioHistoryEventFactory {
     static func get(json: JSON) -> PutioHistoryEvent {
         switch json["type"] {
         case "upload":
-            return PutioUploadEvent(json: json) as PutioHistoryEvent
+            return PutioUploadEvent(json: json)
         case "file_shared":
-            return PutioFileSharedEvent(json: json) as PutioHistoryEvent
+            return PutioFileSharedEvent(json: json)
         case "transfer_completed":
-            return PutioTransferCompletedEvent(json: json) as PutioHistoryEvent
+            return PutioTransferCompletedEvent(json: json)
         case "transfer_error":
             return PutioTransferErrorEvent(json: json)
         case "file_from_rss_deleted_for_space":
@@ -30,6 +30,8 @@ open class PutioHistoryEventFactory {
             return PutioPrivateTorrentPinEvent(json: json)
         case "voucher":
             return PutioVoucherEvent(json: json)
+        case "zip_created":
+            return PutioZipCreatedEvent(json: json)
         default:
             return PutioHistoryEvent(json: json)
         }
